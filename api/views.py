ff6ed6100a5da54694da9c2957fdfeb21e5d21e8
naiyoma <lankas.aurelia@gmail.com>
2024-03-26 13:51:29 +0300

    lnurlwithdraw callback-endpoint

--- api/views.py
+++ api/views.py
@@ -1,24 +1,29 @@
 import grpc
 import os
 import requests
+import json
 import codecs
+import utils
+from lnurl import Lnurl
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import authentication, permissions
 from django.contrib.auth.models import User
 from rest_framework import status
-from lnurl import Lnurl
-import utils
 from django.conf import settings
+from rest_framework.response import Response
+
 
 
 ADMIN_API_KEY = settings.ADMIN_API_KEY
 LNURL_ENDPOINT = settings.LNURL_ENDPOINT
+INVOICE_READ_KEY = settings.INVOICE_READ_KEY
+LNURL_PAYMENTS_ENDPOINT = settings.LNURL_PAYMENTS_ENDPOINT
 
 class RewardView(APIView):
     def generate_lnurl(self, request):
-        # Assuming you extract necessary parameters from the request
+       
         title = request.GET.get("title")
         min_withdrawable = request.GET.get("min_withdrawable")
         max_withdrawable = request.GET.get("max_withdrawable")
@@ -28,7 +33,6 @@ class RewardView(APIView):
         webhook_url = request.GET.get("webhook_url")
         admin_key = request.GET.get("X-Api-Key")
         
-        # Assuming you construct the JSON payload
         payload = {
             "title": "Test1",
             "min_withdrawable": 10,
@@ -54,3 +58,30 @@ class RewardView(APIView):
     def get(self, request):
         # Call the generate_lnurl method
         return self.generate_lnurl(request)
+    
+
+
+class WithdrawCallbackView(APIView):
+    def get(self, request):
+        # Extract k1 token and Lightning invoice from query parameters
+        k1_token = request.GET.get('k1')
+        invoice = request.GET.get('invoice')
+        # create_invoice = {
+        #         "unit": "sat",
+        #         "internal": False,
+        #         "out": False,
+        #         "amount": 10,
+        #         "memo": "Payment memo", 
+        # }
+        # headers = {"Content-type": "application/json", "X-Api-Key": INVOICE_READ_KEY}
+        # response = requests.post(LNURL_PAYMENTS_ENDPOINT, json=create_invoice, headers=headers)
+        # Assuming response.content contains the JSON data
+        # response_data = json.loads(response.content.decode('utf-8'))
+        # payment_request = response_data.get("payment_request")
+        pay_invoice = {
+            "out": True,
+            "bolt11": invoice,
+        }
+        payment_headers = {"Content-type": "application/json", "X-Api-Key": ADMIN_API_KEY}
+        payment_response = requests.post(LNURL_PAYMENTS_ENDPOINT, json=pay_invoice, headers=payment_headers)
+        return Response(payment_response.json())
