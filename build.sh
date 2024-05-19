1252d06ef8d6d3512508a87f00d60a39d74dcd71
ndungudedan <dnkibere@gmail.com>
2024-05-19 21:45:09 +0300

    fix: add createsuperuser command

--- build.sh
+++ build.sh
@@ -5,5 +5,10 @@ set -o errexit
 # Modify this line as needed for your package manager (pip, poetry, etc.)
 pip install -r requirements.txt
 
+if [[ $CREATE_SUPERUSER ]];
+then
+  python manage.py createsuperuser --no-input
+fi
+
 # Apply any outstanding database migrations
 python manage.py migrate
