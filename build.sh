e0eef146922096288a1c5e4498fd1e2be607f01e
ndungudedan <dnkibere@gmail.com>
2024-05-19 21:25:35 +0300

    chore: Redeploy on render

---
+++ build.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+# Exit on error
+set -o errexit
+
+# Modify this line as needed for your package manager (pip, poetry, etc.)
+pip install -r requirements.txt
+
+# Apply any outstanding database migrations
+python manage.py migrate
